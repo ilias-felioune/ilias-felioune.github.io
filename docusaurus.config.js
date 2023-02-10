5caf9b6c172b802dddec024ab1c8fd84fc58b381
ilias-felioune <ilias.felioune@ynov.com>
2023-02-10 09:52:33 +0100

    tests

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   organizationName: 'ilias-felioune', // Usually your GitHub org/user name.
   projectName: 'ilias-felioune.github.io',// Usually your repo name.
   deploymentBranch: 'main', 
-  trailingSlash: false,
+  trailingSlash: false, 
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
